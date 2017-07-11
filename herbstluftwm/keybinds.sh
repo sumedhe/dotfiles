7da68fab8f17b12f86ecf32b5b5ec22431de4200
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-07-12 01:15:47 +0530

    Modify Keybinds

--- herbstluftwm/keybinds.sh
+++ herbstluftwm/keybinds.sh
@@ -5,19 +5,20 @@ hc() {
 }
 
 
-Win=Mod4   # Use the super key as the main modifier
+Mod=Mod4   # Use the super key as the main modifier
+Alt=Mod1
 
-hc keybind $Win-m spawn audacious
-hc keybind $Win-i spawn firefox
-hc keybind $Win-g spawn geary
-hc keybind $Win-t spawn startdict
-hc keybind $Win-b spawn leafpad ~/Documents/Note/quicknotes.txt
-hc keybind $Win-Return spawn x-terminal-emulator
-hc keybind $Win-period spawn ~/myscripts/toggle-swap
-hc keybind $Win-e spawn nautilus
+hc keybind $Mod-$Alt-a spawn audacious
+hc keybind $Mod-$Alt-i spawn firefox
+hc keybind $Mod-$Alt-m spawn geary
+hc keybind $Mod-$Alt-t spawn startdict
+hc keybind $Mod-$Alt-g spawn gitg
+hc keybind $Mod-$Alt-b spawn leafpad ~/Documents/Note/quicknotes.txt
+hc keybind $Mod-Return spawn x-terminal-emulator
+hc keybind $Mod-$Alt-e spawn nautilus
 
 hc keybind Print spawn gnome-screenshot
-hc keybind $Win+Print spawn gnome-screenshot -a
+hc keybind $Mod-$Alt-Print spawn gnome-screenshot -a
 
 
 # Multimedia Keys
@@ -27,5 +28,12 @@ hc keybind XF86AudioMute spawn ~/.config/herbstluftwm/volume.sh mute
 hc keybind XF86MonBrightnessUp spawn gksu "bash -c '/home/sumedhe/.config/herbstluftwm/brightness.sh up'"
 hc keybind XF86MonBrightnessDown spawn gksu "bash -c '/home/sumedhe/.config/herbstluftwm/brightness.sh down'"
 
-
-
+# System
+lock='/opt/i3lock-fancy/lock; sleep 1; xset dpms force off'
+hc keybind $Mod-Control-s spawn systemctl poweroff -i
+hc keybind $Mod-Control-r spawn systemctl reboot
+hc keybind $Mod-Control-r spawn systemctl hibrenate
+hc keybind $Mod-Control-l spawn bash -c $lock
+hc keybind $Mod-Escape spawn bash -c $lock
+hc keybind $Mod-Control-slash spawn glances
+hc keybind $Mod-Control-period spawn ~/myscripts/toggle-swap
