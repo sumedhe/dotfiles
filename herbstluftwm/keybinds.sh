dc3b4eead1cfff29a894ad402ff66a8ceb0280fe
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-07-14 15:01:40 +0530

    Update hlwm keybinds

--- herbstluftwm/keybinds.sh
+++ herbstluftwm/keybinds.sh
@@ -8,17 +8,16 @@ hc() {
 Mod=Mod4   # Use the super key as the main modifier
 Alt=Mod1
 
-hc keybind $Mod-$Alt-a spawn audacious
-hc keybind $Mod-$Alt-i spawn firefox
-hc keybind $Mod-$Alt-m spawn geary
-hc keybind $Mod-$Alt-t spawn startdict
-hc keybind $Mod-$Alt-g spawn gitg
-hc keybind $Mod-$Alt-b spawn leafpad ~/Documents/Note/quicknotes.txt
-hc keybind $Mod-Return spawn x-terminal-emulator
-hc keybind $Mod-$Alt-e spawn nautilus
-
+hc keybind $Alt-a spawn audacious
+hc keybind $Alt-i spawn chromium
+hc keybind $Alt-m spawn geary
+hc keybind $Alt-g spawn gitg
+hc keybind $Alt-b spawn leafpad ~/Documents/Note/quicknotes.txt
+hc keybind $Alt-e spawn nautilus
 hc keybind Print spawn gnome-screenshot
-hc keybind $Mod-$Alt-Print spawn gnome-screenshot -a
+hc keybind $Alt-Print spawn gnome-screenshot -a
+
+hc keybind $Mod-Return spawn x-terminal-emulator
 
 
 # Multimedia Keys
@@ -32,8 +31,15 @@ hc keybind XF86MonBrightnessDown spawn gksu "bash -c '/home/sumedhe/.config/herb
 lock='/opt/i3lock-fancy/lock; sleep 1; xset dpms force off'
 hc keybind $Mod-Control-s spawn systemctl poweroff -i
 hc keybind $Mod-Control-r spawn systemctl reboot
-hc keybind $Mod-Control-r spawn systemctl hibrenate
+hc keybind $Mod-Control-h spawn systemctl hibrenate
 hc keybind $Mod-Control-l spawn bash -c $lock
 hc keybind $Mod-Escape spawn bash -c $lock
+
+# Other
 hc keybind $Mod-Control-slash spawn glances
-hc keybind $Mod-Control-period spawn ~/myscripts/toggle-swap
+hc keybind $Mod-Control-period spawn ~/scripts/toggle-swap
+hc keybind $Mod-d spawn ~/scripts/rofi_run.sh
+hc keybind $Mod-x spawn ~/.config/herbstluftwm/scripts/herbstcommander.sh
+hc keybind $Mod-t spawn ~/.config/herbstluftwm/scripts/toggledualhead.sh
+hc keybind $Mod-grave emit_hook goto_last_tag
+
