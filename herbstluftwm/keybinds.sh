b19eec2674392021aa30172153f72f606f6f551e
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2017-07-18 01:17:32 +0530

    Modify hlwm keybinds

--- herbstluftwm/keybinds.sh
+++ herbstluftwm/keybinds.sh
@@ -9,7 +9,7 @@ Mod=Mod4   # Use the super key as the main modifier
 Alt=Mod1
 
 hc keybind $Alt-a spawn audacious
-hc keybind $Alt-i spawn chromium
+hc keybind $Alt-i spawn firefox
 hc keybind $Alt-m spawn geary
 hc keybind $Alt-g spawn gitg
 hc keybind $Alt-b spawn leafpad ~/Documents/Note/quicknotes.txt
@@ -31,7 +31,7 @@ hc keybind XF86MonBrightnessDown spawn gksu "bash -c '/home/sumedhe/.config/herb
 lock='/opt/i3lock-fancy/lock; sleep 1; xset dpms force off'
 hc keybind $Mod-Control-s spawn systemctl poweroff -i
 hc keybind $Mod-Control-r spawn systemctl reboot
-hc keybind $Mod-Control-h spawn systemctl hibrenate
+hc keybind $Mod-Control-h spawn systemctl hibernate
 hc keybind $Mod-Control-l spawn bash -c $lock
 hc keybind $Mod-Escape spawn bash -c $lock
 
@@ -39,6 +39,7 @@ hc keybind $Mod-Escape spawn bash -c $lock
 hc keybind $Mod-Control-slash spawn glances
 hc keybind $Mod-Control-period spawn ~/scripts/toggle-swap
 hc keybind $Mod-d spawn ~/scripts/rofi_run.sh
+hc keybind $Mod-e spawn ~/scripts/rofi-locate.sh
 hc keybind $Mod-x spawn ~/.config/herbstluftwm/scripts/herbstcommander.sh
 hc keybind $Mod-t spawn ~/.config/herbstluftwm/scripts/toggledualhead.sh
 hc keybind $Mod-grave emit_hook goto_last_tag
